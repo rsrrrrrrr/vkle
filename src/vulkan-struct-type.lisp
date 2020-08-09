750cbc707652023cf470d9e03868e57f87f14b48
aif <1536942665@qq.com>
2020-08-09 16:47:45 +0800

    add more struct and basic type

--- src/vulkan-struct-type.lisp
+++ src/vulkan-struct-type.lisp
@@ -413,3 +413,498 @@
   (:p-next (:pointer :void))
   (:physical-device-count :uint32)
   (:physical-device (:pointer vk-physical-device)))
+
+(defcstruct vk-device-memory-overallocation-create-info-amd
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:overallocation-behavior VkMemoryOverallocationbehavioramd))
+
+(defcstruct vk-device-diagnostics-config-create-info-nv
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-device-diagnostics-config-flags-nv))
+
+(defcstruct vk-device-private-data-create-info-ext
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:private-data-slot-request-count :uint32))
+
+(defcstruct vk-device-queue-global-priority-create-info-ext
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:global-priority VkQueueGlobalPriorityExt))
+
+(defcstruct vk-device-queue-info-2
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-device-queue-create-flags)
+  (:queue-family-index :uint32)
+  (:queue-index :uint32))
+
+(defcstruct vk-command-pool-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-command-pool-create-flags)
+  (:queue-family-index :uint32))
+
+(defcstruct vk-command-buffer-allocate-infp
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:command-pool vk-command-pool)
+  (:level VKCommandbufferlevel)
+  (:command-buffer-count :uint32))
+
+(defcstruct vk-command-buffer-inheritance-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:render-pass vk-render-pass)
+  (:sub-pass :uint32)
+  (:framebuffer vk-framebuffer)
+  (:occlusion-query-enable vk-bool-32)
+  (:flags vk-query-control-flags)
+  (:pipeline-statistics vk-query-pipeline-statistic-flags))
+
+(defcstruct vk-command-buffer-begin-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-command-buffer-usage-flags)
+  (:inheritance-info (:pointer (:struct vk-command-buffer-inheritance-info))))
+
+(defcstruct vk-command-buffer-inheritance-conditional-rendering-info-ext
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:conditional-rendering-enable vk-bool-32))
+
+(defcstruct vk-command-buffer-inheritance-render-pass-transform-info-qcom
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:transform VkSurfaceTransformflagbitskhr)
+  (:render-area (:struct vk-rect-2d)))
+
+(defcstruct vk-submit-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:wait-semaphore-count :uint32)
+  (:wait-semaphore (:pointer vk-semaphore))
+  (:wait-dst-stage-mask (:pointer vk-pipline-stage-flags))
+  (:command-buffer-count :uint32)
+  (:command-buffer (:pointer vk-command-buffer))
+  (:signal-semaphore-count :uint32)
+  (:signal-semaphore (:pointer vk-semaphore)))
+
+(defcstruct vk-timeline-semaphore-submit-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:wait-semaphore-value-count :uint32)
+  (:wait-semaphore-values (:pointer :uint64))
+  (:signal-semaphore-count :uint32)
+  (:signal-semaphore-values (:pointer :uint64)))
+
+(defcstruct vk-d3d12-fence-submit-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:wait-semaphore-value-count :uint32)
+  (:wait-semaphore-values (:pointer :uint64))
+  (:signal-semaphore-count :uint32)
+  (:signal-semaphore-values (:pointer :uint64)))
+
+(defcstruct vk-win32-keyed-mutex-acquire-release-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:acquire-count :uint32)
+  (:acquire-syncs vk-device-memory)
+  (:acquire-keys (:pointer :uint64))
+  (:acquire-timeout (:pointer :uint32))
+  (:release-count :uint32)
+  (:release-syncs (:pointer vk-device-memory))
+  (:release-keys (:pointer :uint64)))
+
+(defcstruct vk-win32-keyed-mutex-acquire-release-info-nv
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:acquire-count :uint32)
+  (:acquire-syncs vk-device-memory)
+  (:acquire-keys (:pointer :uint64))
+  (:acquire-timeout (:pointer :uint32))
+  (:release-count :uint32)
+  (:release-syncs (:pointer vk-device-memory))
+  (:release-keys (:pointer :uint64)))
+
+(defcstruct vk-protected-submit-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:protected-submit vk-bool-32))
+
+(defcstruct vk-device-group-submit-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:wait-semaphore-count :uint32)
+  (:wait-semaphore-device-indices (:pointer :uint32))
+  (:command-buffer-count :uint32)
+  (:command-buffer-device-masks (:pointer :uint32))
+  (:signal-semaphore-count :uint32)
+  (:signal-semaphore-device-indices (:pointer :uint32)))
+
+(defcstruct vk-performance-query-submit-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:count-pass-index :uint32))
+
+(defcstruct vk-device-group-command-buffer-begin-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:device-mask :uint32))
+
+(defcstruct vk-fence-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-fence-create-flags))
+
+(defcstruct vk-export-fence-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-external-fence-handle-type-flags))
+
+;;in windows 
+(defcstruct security-attributes
+  (:length :uint32)
+  (:lp-void (:pointer :void))
+  (:bool :int32))
+
+(defcstruct vk-export-fence-win32-handle-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:attributes (:pointer (:struct security-attributes)))
+  (:dw-access :uint32)
+  (:name (:pointer :uint16)))
+
+(defcstruct vk-fence-get-win32-handle-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:fence vk-fence)
+  (:handle-type VkExternalFenceHandleTypeFlagBits))
+;;end windows
+
+(defcstruct vk-fence-get-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:fence vk-fence)
+  (:handle-type VkExternalFenceHandleTypeFlagBits))
+
+(defcstruct vk-device-event-info-ext
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:device-event VkDeviceEventTypeExt))
+
+(defcstruct vk-display-event-info-ext
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:display-event VkDisplayEventTypeExt))
+
+(defcstruct vk-import-fence-win32-handle-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:fence vk-fence)
+  (:flags vk-fence-import-flags)
+  (:handle-type VkExternalFenceFeatureFlagBits)
+  (:handle (:pointer :void))
+  (:name (:pointer :uint16)))
+
+(defcstruct vk-import-fence-fd-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:fence vk-fence)
+  (:flags vk-fence-import-flags)
+  (:handle-type VkExternalFenceFeatureFlagBits)
+  (:fd :int))
+
+(defcstruct vk-semaphore-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-semaphore-create-flags))
+
+(defcstruct vk-semaphore-type-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:semaphore-type VkSemaphoreType)
+  (:initial-value :uint64))
+
+(defcstruct vk-export-semaphore-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:handle-type vk-external-semaphore-handle-type-flags))
+
+(defcstruct vk-export-semaphore-win32-handle-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:attributes (:pointer (:struct security-attributes)))
+  (:dw-access :uint32)
+  (:name (:pointer :uint16)))
+
+(defcstruct vk-semaphore-get-win32-handle-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:semaphore vk-semaphore)
+  (:handle-type VkExternalSemaphoreHandleTypeFlagbits))
+
+(defcstruct vk-semaphore-get-fd-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:semaphore vk-semaphore)
+  (:handle-type VkExternalSemaphoreHandleTypeFlagbits))
+
+(defcstruct vk-semaphore-wait-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-semaphore-wait-flags)
+  (:semaphore-count :uint32)
+  (:semaphores (:pointer vk-semaphore))
+  (:value (:pointer :uint64)))
+
+(defcstruct vk-semaphore-signal-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:semaphore vk-semaphore)
+  (:value :uint64))
+
+(defcstruct vk-import-semaphore-win32-handle-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:semaphore vk-semaphore)
+  (:flags vk-semaphore-import-flags)
+  (:handle-type VkExternalSemaphoreHandleTypeFlagBits)
+  (:handle (:pointer :void))
+  (:name (:pointer :uint16)))
+
+(defcstruct vk-import-semaphore-fd-info-khr
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:semaphore vk-semaphore)
+  (:flags vk-semaphore-import-flags)
+  (:handle-type VkExternalSemaphoreHandleTypeFlagBits)
+  (:fd :int))
+
+(defcstruct vk-event-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-event-create-flags))
+
+(defcstruct vk-memory-barrier
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:src-access-mask vk-access-flags)
+  (:dst-access-mask vk-access-flags))
+
+(defcstruct vk-buffer-memory-barrier
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:src-access-mask vk-access-flags)
+  (:dst-access-mask vk-access-flags)
+  (:src-queue-family-index :uint32)
+  (:dst-queue-family-index :uint32)
+  (:buffer vk-buffer)
+  (:offset vk-device-size)
+  (:size vk-device-size))
+
+(defcstruct vk-image-subresource-range
+  (:aspect-mask vk-image-aspect-flags)
+  (:base-mip-level :uint32)
+  (:level-count :uint32)
+  (:base-array-layer :uint32)
+  (:layer-count :uint32))
+
+(defcstruct vk-buffer-memory-barrier
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:src-access-mask vk-access-flags)
+  (:dst-access-mask vk-access-flags)
+  (:old-layout VkImageLayout)
+  (:new-layout VkImageLayout)
+  (:src-queue-family-index :uint32)
+  (:dst-queue-family-index :uint32)
+  (:image vk-image)
+  (:sub-resource-range (:struct vk-image-subresource-range)))
+
+(defcstruct vk-calibrated-timestamp-info-ext
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:time-domain VkTimedomainext))
+
+(defcstruct vk-attachment-description
+  (:flags vk-attachment-description-flags)
+  (:format VkFormat)
+  (:samples VkSampleCountFlagbits)
+  (:load-op VkAttachmentLoadop)
+  (:store-op VkAttachmentStoreop)
+  (:stencil-load-op VkAttachmentLoadop)
+  (:stencil-store-op VkAttachmentStoreop)
+  (:initial-layout VkImageLayout)
+  (:final-layout VkImageLayout))
+
+(defcstruct vk-attachment-reference
+  (:attachment :uint32)
+  (:layout VkImageLayout))
+
+(defcstruct vk-subpass-description
+  (:flags vk-subpass-description-flags)
+  (:pipeline-bind-point VkPipelineBindpoint)
+  (:input-attachment-cout :uint32)
+  (:input-attachment (:pointer (:struct vk-attachment-reference)))
+  (:color-attachment-count :uint32)
+  (:color-attachments (:pointer (:struct vk-attachment-reference)))
+  (:reslove-attachments (:pointer (:struct vk-attachment-reference)))
+  (:depth-stencil-attachments (:pointer (:struct vk-attachment-reference)))
+  (:reserve-attachment-count :uint32)
+  (:preserve-attachments (:pointer :uint32)))
+
+(defcstruct vk-subpass-dependency
+  (:src-subpass :uint32)
+  (:dst-subpass :uint32)
+  (:src-stage-mask vk-pipline-stage-flags)
+  (:dst-stage-mask vk-pipline-stage-flags)
+  (:src-access-mask vk-access-flags)
+  (:dst-access-mask vk-access-flags)
+  (:dependency-flags vk-dependency-flags))
+
+(defcstruct vk-render-pass-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-render-pass-create-flags)
+  (:attachment-count :uint32)
+  (:attachment (:pointer (:struct vk-attachment-description)))
+  (:subpass-count :uint32)
+  (:subpasses (:pointer (:struct vk-subpass-description)))
+  (:dependency-count :uint32)
+  (:dependenies (:pointer (:struct vk-subpass-dependency))))
+
+(defcstruct vk-render-pass-multiview-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:subpass-count :uint32)
+  (:view-mask (:pointer :uint32))
+  (:dependency-count :uint32)
+  (:view-offsets (:pointer :uint32))
+  (:correlation-mask-count :uint32)
+  (:correlation-mask (:pointer :uint32)))
+
+(defcstruct vk-render-pass-fragment-density-map-create-info-ext
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:fragment-density-map-attachment (:struct vk-attachment-reference)))
+
+(defcstruct vk-input-attachment-aspect-reference
+  (:subpass :uint32)
+  (:input-attachment-index :uint32)
+  (:aspect-mask vk-image-aspect-flags))
+
+(defcstruct vk-render-pass-input-attachment-aspect-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:aspect-reference-count :uint32)
+  (:aspect-reference (:pointer (:struct vk-input-attachment-aspect-reference))))
+
+(defcstruct vk-attachment-description-2
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-attachment-description-flags)
+  (:format VkFormat)
+  (:samples VkSampleCountFlagbits)
+  (:load-op VkAttachmentLoadop)
+  (:store-op VkAttachmentStoreop)
+  (:stencil-load-op VkAttachmentLoadop)
+  (:stencil-store-op VkAttachmentStoreop)
+  (:initial-layout VkImageLayout)
+  (:final-layout VkImageLayout))
+
+(defcstruct vk-attachment-reference-2
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:attachment :uint32)
+  (:layout VkImageLayout)
+  (:aspect-mask vk-image-aspect-flags))
+
+(defcstruct vk-subpass-description-2
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-subpass-description-flags)
+  (:pipeline-bind-point VkPipelineBindpoint)
+  (:view-mask :uint32)
+  (:input-attachment-cout :uint32)
+  (:input-attachment (:pointer (:struct vk-attachment-reference-2)))
+  (:color-attachment-count :uint32)
+  (:color-attachments (:pointer (:struct vk-attachment-reference-2)))
+  (:reslove-attachments (:pointer (:struct vk-attachment-reference-2)))
+  (:depth-stencil-attachments (:pointer (:struct vk-attachment-reference-2)))
+  (:reserve-attachment-count :uint32)
+  (:preserve-attachments (:pointer :uint32)))
+
+(defcstruct vk-subpass-dependency-2
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:src-subpass :uint32)
+  (:dst-subpass :uint32)
+  (:src-stage-mask vk-pipline-stage-flags)
+  (:dst-stage-mask vk-pipline-stage-flags)
+  (:src-access-mask vk-access-flags)
+  (:dst-access-mask vk-access-flags)
+  (:dependency-flags vk-dependency-flags)
+  (:view-offset :uint32))
+
+(defcstruct vk-render-pass-create-info-2
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-render-pass-create-flags)
+  (:attachment-count :uint32)
+  (:attachment (:pointer (:struct vk-attachment-description-2)))
+  (:subpass-count :uint32)
+  (:subpasses (:pointer (:struct vk-subpass-description-2)))
+  (:dependency-count :uint32)
+  (:dependenies (:pointer (:struct vk-subpass-dependency-2)))
+  (:correlate-view-mask-count :uint32)
+  (:correlated-view-masks (:pointer :uint32)))
+
+(defcstruct vk-attachment-description-stenci-layout
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:stencil-initial-layout VkImageLayout)
+  (:stencil-final-layout VkImageLayout))
+
+(defcstruct vk-subpass-description-depth-stencil-reslove
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:depth-reslove-mode VkResolveModeFlagbits)
+  (:stencil-reslove-mode VkResolveModeFlagbits)
+  (:depth-stencil-resolve-attachment (:struct vk-attachment-reference-2)))
+
+(defcstruct vk-attachment-reference-stencil-layout
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:stencil-layout VkImageLayout))
+
+(defcstruct vk-framebuffer-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-framebuffer-create-flags)
+  (:render-pass vk-render-pass)
+  (:attachment-count :uint32)
+  (:attachments (:pointer vk-image-view))
+  (:width :uint32)
+  (:height :uint32)
+  (:layers :uint32))
+
+(defcstruct vk-framebuffer-attachment-image-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:flags vk-image-create-flags)
+  (:usage vk-image-usage-flags)
+  (:width :uint32)
+  (:height :uint32)
+  (:layer-count :uint32)
+  (:voew-format-count :uint32)
+  (:view-formats (:pointer VkFormat)))
+
+(defcstruct vk-framebuffer-attachments-create-info
+  (:type VkStructureType)
+  (:p-next (:pointer :void))
+  (:attachment-image-info-count :uint32)
+  (:attachment-image-infos (:pointer (:struct vk-framebuffer-attachment-image-info))))
