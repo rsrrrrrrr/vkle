2102da21b44552f7e999c57571a8ad463d94c728
aif <1536942665@qq.com>
2020-09-26 18:53:58 +0800

    delete something

--- src/vkle.lisp
+++ src/vkle.lisp
@@ -1,1 +1,16 @@
 (in-package :vkle)
+
+(export '(make-vulkan-version))
+
+(declaim (inline make-vulkan-version))
+
+(defun make-vulkan-version (&optional (major 1) (minor 2) (patch 0))
+  (logior (ash major 22)
+	  (ash minor 12)
+	  patch))
+
+(defmacro define-translation (lisp-struct-name c-struct-name)
+  (eval `(define-foreign-type ,lisp--struct-name ()
+	   ()
+	   (:actual-type :struct ,c-struct-name)
+	   (:simple-parser obj))))
--- vkle-demo.asd
+++
@@ -1,5 +0,0 @@
-(asdf:defsystem :vkle-demo
-  :depends-on (:cl-glfw3 :vkle)
-  :serial t
-  :pathname "demo"
-  :components ((:file "test")))
